42c46391694067b7a6a077e77a6c85fceac66c03
Aashay2112 <2k23.csaiml2313753@gmail.com>
2025-10-24 19:38:00 +0530

    Updated for vercel

--- Server/server.js
+++ Server/server.js
@@ -52,9 +52,12 @@ app.use("/api/messages", messageRouter);
 
 // Connect DB
 await connectDB();
-
+if(process.env.NODE_ENV!=="production"){
 const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => {
   console.log(`Server running on PORT: ${PORT}`);
 });
+}
+//exporting server fro vercel
+export default server;
